d5ecd9776728f2aa0ffee14d34717c15fb394eba
Jonathan <gucs.net@gmail.com>
2017-11-21 18:47:27 +0800

    Alseye Demo

---
+++ AlseyeDemo/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace AlseyeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(292, 251);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.BottomToolStripPanel
+            // 
+            this.toolStripContainer1.BottomToolStripPanel.Controls.Add(this.statusStrip1);
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.textBox1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(292, 251);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(292, 273);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(292, 22);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Alseye Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ AlseyeDemo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+
+namespace AlseyeDemo
+{
+    public partial class Form1 : Form
+    {
+        private readonly Computer _computer = new Computer
+        {
+            CPUEnabled = true,
+            GPUEnabled = true,
+            HDDEnabled = true,
+            MainboardEnabled = true,
+            RAMEnabled = true,
+            FanControllerEnabled = true
+        };
+
+        private readonly List<IHardware> _hardwares = new List<IHardware>();
+        private readonly List<ISensor> _sensors = new List<ISensor>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _computer.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Show();
+
+            toolStripStatusLabel1.Text = @"Loading ...";
+            toolStripStatusLabel1.Invalidate();
+            var sw = new Stopwatch();
+            sw.Start();
+            _computer.Open();
+            sw.Stop();
+            toolStripStatusLabel1.Text = $@"Loaded in {sw.Elapsed.TotalSeconds} seconds.";
+
+            InitVar();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateHardware();
+
+            textBox1.Text = GetText();
+        }
+
+        private void InitVar()
+        {
+            LoopHardware(_computer.Hardware);
+            _sensors.AddRange(from hw in _hardwares
+                from sensor in hw.Sensors
+                select sensor);
+        }
+
+        private void UpdateHardware()
+        {
+            foreach (var hw in _hardwares)
+                hw.Update();
+        }
+
+        private string GetText()
+        {
+            // group by Hardware Name and Sensor Type
+            var q = from sensor in _sensors
+                where sensor.SensorType == SensorType.Temperature || sensor.SensorType == SensorType.Load
+                group sensor by sensor.Hardware.Name
+                into sg
+                select new
+                {
+                    Name = sg.Key,
+                    Types = from ss in sg.ToList()
+                    group ss by ss.SensorType
+                    into ssg
+                    select new {Type = ssg.Key, Sensors = ssg.ToList()}
+                };
+
+            // build string
+            var sb = new StringBuilder();
+            foreach (var sensorGroup in q)
+            {
+                sb.AppendLine(sensorGroup.Name);
+
+                foreach (var type in sensorGroup.Types)
+                {
+                    sb.Append(new string(' ', 2));
+                    sb.AppendLine(type.Type.ToString());
+
+                    foreach (var sensor in type.Sensors)
+                    {
+                        sb.AppendFormat("{0}{1}, Value = {2}, Min = {3}, Max = {4}",
+                            new string(' ', 4), sensor.Name, sensor.Value, sensor.Min, sensor.Max);
+                        sb.AppendLine();
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private void LoopHardware(IHardware[] hardwares)
+        {
+            foreach (var hw in hardwares)
+            {
+                if (hw.SubHardware.Length == 0)
+                {
+                    _hardwares.Add(hw);
+                    continue;
+                }
+                LoopHardware(hw.SubHardware);
+            }
+        }
+    }
+}
